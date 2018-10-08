95c3b546880b00a3d5be2aaa6162f61fa9fc8f23
zhouhao <zh.sqy@qq.com>
2018-10-08 18:37:21 +0800

    增加 自动配置

--- redis-manager-restful/src/main/java/org/hswebframework/redis/manager/restful/RestfulRedisManagerAutoConfiguration.java
+++ redis-manager-restful/src/main/java/org/hswebframework/redis/manager/restful/RestfulRedisManagerAutoConfiguration.java
@@ -1,5 +1,10 @@
 package org.hswebframework.redis.manager.restful;
 
+import org.hswebframework.redis.manager.InMemoryRedisClientRepository;
+import org.hswebframework.redis.manager.RedisClientRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +16,10 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("org.hswebframework.redis.manager.restful")
 public class RestfulRedisManagerAutoConfiguration {
 
-
+    @Bean
+    @ConditionalOnMissingBean(RedisClientRepository.class)
+    @ConfigurationProperties(prefix = "redis.manager")
+    public RedisClientRepository inMemoryRedisClientRepository() {
+        return new InMemoryRedisClientRepository();
+    }
 }
