2e9c3e5c085c90d1007b9bad89380c61f9086d80
zhouhao <zh.sqy@qq.com>
2018-10-08 14:46:53 +0800

    添加自动配置

---
+++ redis-manager-restful/src/main/java/org/hswebframework/redis/manager/restful/RestfulRedisManagerAutoConfiguration.java
@@ -0,0 +1,15 @@
+package org.hswebframework.redis.manager.restful;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhouhao
+ * @since 1.0.0
+ */
+@Configuration
+@ComponentScan("org.hswebframework.redis.manager.restful")
+public class RestfulRedisManagerAutoConfiguration {
+
+
+}
